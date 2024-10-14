8ddf15396397bc7a03e01c7821110128f7daddc1
Hoang Trieu <hoangtrieuhuynh22@augustana.edu>
2024-10-13 22:11:05 -0500

    Update HamUI Controller and App

--- HamRadio/src/main/java/edu/augustana/App.java
+++ HamRadio/src/main/java/edu/augustana/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,8 +19,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("WelcomeScreen"), 640, 480);
+        scene = new Scene(new BorderPane(), 640, 480);
         stage.setScene(scene);
+        switchToMainView();
         stage.show();
     }
 
@@ -29,6 +32,29 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    private static void switchToView(String fxmlFileName) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxmlFileName));
+            scene.setRoot(fxmlLoader.load());
+        } catch (IOException ex) {
+            System.err.println("Can't find FXML file " + fxmlFileName);
+            ex.printStackTrace();
+        }
+
+    }
+
+    public static void switchToMainView() {
+        switchToView("WelcomeScreen.fxml");
+    }
+
+    public static void switchToRegisterView() {
+        switchToView("WelcomeScreen.fxml");
+    }
+
+    public static void switchToLogInView() {
+        switchToView("WelcomeScreen.fxml");
     }
 
     public static void main(String[] args) {
--- HamRadio/src/main/java/edu/augustana/HamUIController.java
+++ HamRadio/src/main/java/edu/augustana/HamUIController.java
@@ -1,12 +1,50 @@
 package edu.augustana;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.shape.Circle;
 
 public class HamUIController {
+    @FXML
+    private Slider chanelSlider;
 
     @FXML
-    private void switchToPrimary() throws IOException {
+    private Button clarifierDown;
+
+    @FXML
+    private Button clarifierUp;
+
+    @FXML
+    private Button helpBtn;
+
+    @FXML
+    private Button moreBtn;
+
+    @FXML
+    private Circle recordBtn;
+
+    @FXML
+    private Button returnBtn;
+
+    @FXML
+    private Button trainingBtn;
+
+    @FXML
+    private Button volumeDownBtn;
+
+    @FXML
+    private Button volumeUpBtn;
+
+
+    @FXML
+    private void switchToWelcomeScreen() throws IOException {
         App.setRoot("WelcomeScreen");
     }
+
+
+
 }
